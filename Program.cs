2381bb078e698330c7b2235d7680f4411190b27c
Gihun Ham <progh2@gmail.com>
2025-06-12 14:05:44 +0900

    상속 시 생성자 예

--- Program.cs
+++ Program.cs
@@ -8,8 +8,29 @@ namespace ClassExample32
 {
     class Program
     {
+        class Parent
+        {
+            public Parent() {  Console.WriteLine("Parent()");}
+            public Parent(int param) { Console.WriteLine("Parent(int param)"); }
+            public Parent(string param) { Console.WriteLine("Parent(string param)"); }
+        }
+
+        class Child : Parent
+        {
+            public Child() : base(10)
+            {
+                Console.WriteLine("Child():base(10)");
+            }
+            public Child(string input) : base(input)
+            {
+                Console.WriteLine("Child(string input) : base(input)");
+            }
+        }
+
         static void Main(string[] args)
         {
+            Child childA = new Child();
+            Child childB = new Child("string");
         }
     }
 }
